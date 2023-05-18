2fa8a65f22b64e90e0a0f9f21393a8b8d5ee0697
Nandyala M Reddy TPR <nandyala.m.reddy.tpr@vialto.com>
2023-05-18 16:01:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import FormComponent from './FormComponent';
+function App() {
+  return (
+    <div className="App">
+      <FormComponent />
+    </div>
+  );
+}
+export default App;
+
---
+++ src/FormComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { FormBuilder } from 'react-formio';
+function FormComponent() {
+  return <FormBuilder />;
+}
+export default FormComponent;
+
